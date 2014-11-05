f828d3d502fd8ba118c08118a0e68e480ad0741e
Sudipta <sudipta.besu@gmail.com>
2014-11-05 08:48:25 -0500

    Beginning my new application

---
+++ include/base/CrowApp.h
@@ -0,0 +1,22 @@
+#ifndef CROWAPP_H
+#define CROWAPP_H
+
+#include "MooseApp.h"
+
+class CrowApp;
+
+template<>
+InputParameters validParams<CrowApp>();
+
+class CrowApp : public MooseApp
+{
+public:
+  CrowApp(const std::string & name, InputParameters parameters);
+  virtual ~CrowApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* CROWAPP_H */
--- src/base/CrowApp.C
+++ src/base/CrowApp.C
@@ -1,45 +1,45 @@
-#include "StorkApp.h"
+#include "CrowApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
 #include "ModulesApp.h"
 
 template<>
-InputParameters validParams<StorkApp>()
+InputParameters validParams<CrowApp>()
 {
   InputParameters params = validParams<MooseApp>();
   return params;
 }
 
-StorkApp::StorkApp(const std::string & name, InputParameters parameters) :
+CrowApp::CrowApp(const std::string & name, InputParameters parameters) :
     MooseApp(name, parameters)
 {
   srand(processor_id());
 
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
-  StorkApp::registerObjects(_factory);
+  CrowApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
   ModulesApp::associateSyntax(_syntax, _action_factory);
-  StorkApp::associateSyntax(_syntax, _action_factory);
+  CrowApp::associateSyntax(_syntax, _action_factory);
 }
 
-StorkApp::~StorkApp()
+CrowApp::~CrowApp()
 {
 }
 
 void
-StorkApp::registerApps()
+CrowApp::registerApps()
 {
-  registerApp(StorkApp);
+  registerApp(CrowApp);
 }
 
 void
-StorkApp::registerObjects(Factory & factory)
+CrowApp::registerObjects(Factory & factory)
 {
 }
 
 void
-StorkApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+CrowApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
 {
 }

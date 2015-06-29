cdf3882adb7f53c8218bd7b2efcbb5553e2f2a68
Sudipta Biswas <biswas7@purdue.edu>
2015-06-29 08:52:40 -0600

    adding action for specified smooth circle IC

--- include/actions/MultiSmoothParticleICAction.h
+++ include/actions/MultiSmoothParticleICAction.h
@@ -4,14 +4,14 @@
 /*          All contents are licensed under LGPL V2.1           */
 /*             See LICENSE for full restrictions                */
 /****************************************************************/
-#ifndef POLYCRYSTALHEXGRAINICACTION_H
-#define POLYCRYSTALHEXGRAINICACTION_H
+#ifndef MULTISMOOTHPARTICLEICACTION_H
+#define MULTISMOOTHPARTICLEICACTION_H
 
 #include "InputParameters.h"
 #include "Action.h"
 
 /**
- * Automatically generates ic for polycrystal hexagonal grain structure. Must have squared number of grains and periodic BCs.
+ * Automatically generates ic for polycrystal circular grain structure at specified locations and assign order parameters.
  */
 class MultiSmoothParticleICAction: public Action
 {
@@ -29,23 +29,14 @@ private:
   std::vector<Real> _y_positions;
   std::vector<Real> _z_positions;
   std::vector<Real> _input_radii;
-  
+
   Real _int_width;
   bool _3D_spheres;
   unsigned int _num_dim;
-
-  //unsigned int _numbub;
-  //Real _bubspac;
-
-  //unsigned int _numtries;
-  //Real _radius;
-  //Real _radius_variation;
-  //MooseEnum _radius_variation_type;
-  //bool _avoid_bounds;
 
 };
 
 template<>
 InputParameters validParams<MultiSmoothParticleICAction>();
 
-#endif //POLYCRYSTALHEXGRAINICACTION_H
+#endif //MULTISMOOTHPARTICLEICACTION_H
--- src/actions/MultiSmoothParticleICAction.C
+++ src/actions/MultiSmoothParticleICAction.C
@@ -35,17 +35,6 @@ InputParameters validParams<MultiSmoothParticleICAction>()
   params.addRequiredParam<std::vector<Real> >("radii", "The radius for each circle");
   params.addParam<Real>("int_width", 0.0, "The interfacial width of the void surface.  Defaults to sharp interface");
   params.addParam<bool>("3D_spheres", true, "in 3D, whether the objects are spheres or columns");
-  //params.addRequiredParam<unsigned int>("numbub", "The number of bubbles to be placed on GB");
-  //params.addRequiredParam<Real>("bubspac", "minimum spacing of bubbles, measured from center to center");
-  //params.addParam<unsigned int>("rand_seed", 2000, "random seed");
-  //params.addParam<unsigned int>("numtries", 1000, "The number of tries");
-  //params.addRequiredParam<Real>("radius", "Mean radius value for the circels");
-  //params.addParam<Real>("radius_variation", 0.0, "Plus or minus fraction of random variation in the bubble //radius for uniform, standard deviation for normal");
-  //MooseEnum rand_options("uniform normal none","none");
-  //params.addParam<MooseEnum>("radius_variation_type", rand_options, "Type of distribution that random circle //radii will follow");
-  //params.addParam<bool>("avoid_bounds", true, "Don't place any bubbles on the simulation cell boundaries");
-
-
   return params;
 }
 
@@ -60,13 +49,6 @@ MultiSmoothParticleICAction::MultiSmoothParticleICAction(const std::string & nam
     _int_width(getParam<Real>("int_width")),
     _3D_spheres(getParam<bool>("3D_spheres")),
     _num_dim(_3D_spheres ? 3 : 2)
-    //_numbub(getParam<unsigned int>("numbub")),
-    //_bubspac(getParam<Real>("bubspac")),
-    //_numtries(getParam<unsigned int>("numtries")),
-    //_radius(getParam<Real>("radius")),
-    //_radius_variation(getParam<Real>("radius_variation")),
-    //_radius_variation_type(getParam<MooseEnum>("radius_variation_type")),
-    //_avoid_bounds(getParam<bool>("avoid_bounds"))
 {
 }
 
@@ -85,11 +67,10 @@ MultiSmoothParticleICAction::act()
     std::stringstream out;
     out << op;
     var_name.append(out.str());
-    //_radii.resize(_op_num);
-    //Set parameters for BoundingBoxIC
+
+    //Set parameters for SmoothCircleIC
     InputParameters poly_params = _factory.getValidParams("SmoothCircleIC");
     poly_params.set<VariableName>("variable") = var_name;
-    //poly_params.set<unsigned int>("numbub") = _op_num;
     poly_params.set<Real>("x1") = _x_positions[op];
     poly_params.set<Real>("y1") = _y_positions[op];
     if(_num_dim == 3)
@@ -101,22 +82,9 @@ MultiSmoothParticleICAction::act()
     poly_params.set<unsigned int>("op_index") = op;
     poly_params.set<Real>("int_width") = _int_width;
     poly_params.set<bool>("3D_spheres") = _3D_spheres;
-    //poly_params.set<unsigned int>("rand_seed") = getParam<unsigned int>("rand_seed");
-    //poly_params.set<unsigned int>("numbub") = _numbub ;
-    //poly_params.set<Real>("bubspac") = _bubspac ;
-    //poly_params.set<unsigned int>("numtries") = _numtries;
-    //poly_params.set<Real>("radius") = _radius;
-    //poly_params.set<Real>("radius_variation") = _radius_variation;
-    //poly_params.set<MooseEnum>("radius_variation_type") = _radius_variation_type;
-    //poly_params.set<bool>("avoid_bounds") = _avoid_bounds;
-    //unsigned int n = 0;
-    //if(n == op)
-    //{
-      //Values for circle grain
-      poly_params.set<Real>("invalue") = 1.0;
-      poly_params.set<Real>("outvalue") = 0.0;
-    //}
-    
+    //Values for circle grain
+    poly_params.set<Real>("invalue") = 1.0;
+    poly_params.set<Real>("outvalue") = 0.0;
 
     //Add initial condition
     _problem->addInitialCondition("SmoothCircleIC", "InitialCondition", poly_params);
